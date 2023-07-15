0e25a96b7fda15d5a58ab66c7b49d25f14e4f89b
tupudipi <tupudipi@gmail.com>
2023-07-15 15:14:29 +0300

    linking to firebase

---
+++ pages/api/login.js
@@ -0,0 +1,45 @@
+import getPool from '../../lib/db';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from '../../firebase';
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).end();
+    }
+
+    const { username, parola } = req.body;
+
+    const pool = getPool();
+
+    try {
+        // Check if username or email already exists
+        const [existingUsers] = await pool.query(
+            'SELECT * FROM users WHERE username = ?',
+            [username]
+        );
+
+        if (existingUsers.length) {
+            const user = existingUsers[0];
+            const email = user.email;
+            const nume = user.nume;
+            const prenume = user.prenume;
+            const rol = user.rol;
+
+            const auth = getAuth();
+            signInWithEmailAndPassword(auth, email, parola)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    return res.status(200).json({ message: `User ${username} logged in successfully`, user: { username, nume, prenume, email, rol } });
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'An error occurred during registration' });
+    }
+
+}
--- pages/api/register.js
+++ pages/api/register.js
@@ -1,6 +1,6 @@
-// pages/api/register.js
-import bcrypt from 'bcryptjs';
 import getPool from '../../lib/db';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from '../../firebase';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
@@ -9,20 +9,43 @@ export default async function handler(req, res) {
 
   const { username, nume, prenume, email, parola } = req.body;
 
+  // Check the password against the same requirements as the front-end
+  const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
+  if (!passwordPattern.test(parola)) {
+    return res.status(400).json({ message: 'Password does not meet requirements.' });
+  }
+
   const pool = getPool();
 
   try {
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(parola, salt);
-
-    await pool.query(
-      'INSERT INTO users (username, nume, prenume, email, parola, rol) VALUES (?, ?, ?, ?, ?, ?)',
-      [username, nume, prenume, email, hashedPassword, 'user']
+    // Check if username or email already exists
+    const [existingUsers] = await pool.query(
+      'SELECT * FROM users WHERE username = ? OR email = ?',
+      [username, email]
     );
+
+    if (existingUsers.length) {
+      return res.status(409).json({ message: 'Username or email already in use' });
+    }
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, parola)
+      .then(async (userCredential) => {
+        const user = userCredential.user;
+        await pool.query(
+          'INSERT INTO users (username, nume, prenume, email, rol) VALUES (?, ?, ?, ?, ?)',
+          [username, nume, prenume, email, 'user']
+        );
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
 
     return res.status(200).json({ message: 'User registered successfully' });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'An error occurred during registration' });
   }
+
 }
