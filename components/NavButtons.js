bbd98591c97379c4c70ef0948d74df1eaa9d48d2
tupudipi <tupudipi@gmail.com>
2023-07-15 15:17:41 +0300

    blabla

--- components/NavButtons.js
+++ components/NavButtons.js
@@ -2,20 +2,20 @@ import RegistrationModal from './RegistratioModal';
 import LoginModal from './LoginModal';
 import { Button } from 'react-bootstrap';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import app from '../firebase';
+import app from '../src/app/firebase';
+
 
 const NavButtons = () => {
-    const [user, setUser] = useState(null);
+
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
       if (user) {  
-        setUser(user); 
             return (
                 <div className='d-flex flex-wrap justify-content-center'>
                     <div className="d-flex">
                         <div className="mx-2">
                             <Button variant="outline-primary" className="rounded-pill border-2" disabled>
-                                {email}
+                                {user.email}
                             </Button>
                         </div>
                         <div className="mx-2">
