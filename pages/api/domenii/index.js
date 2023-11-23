75bb833df256af0886832a362eb12a6ec8924ff6
tupudipi <tupudipi@gmail.com>
2023-11-23 12:09:31 +0200

    pg pool

--- pages/api/domenii/index.js
+++ pages/api/domenii/index.js
@@ -5,17 +5,16 @@ import getPool from '../../../lib/db';
 export default async function handler(req, res) {
   const pool = getPool();
 
-  let connection;
+  // let connection;
   try {
-    connection = await pool.connect();
-    const [rows, fields] = await connection.query('SELECT * FROM domeniistudiu');
+    const [rows, fields] = await pool.query('SELECT * FROM domeniistudiu');
     res.json(rows);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
   } finally {
-    if (connection) {
-      connection.release();
+    if (pool) {
+      await pool.end();
     }
   }
 }
