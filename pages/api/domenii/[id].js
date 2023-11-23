53d270423884e83a546146be1f07b20b2e6b5855
tupudipi <tupudipi@gmail.com>
2023-11-23 14:50:13 +0200

    api single domeniu

--- pages/api/domenii/[id].js
+++ pages/api/domenii/[id].js
@@ -7,10 +7,8 @@ export default async function handler(req, res) {
 
   const { id } = req.query;
 
-  let connection;
   try {
-    connection = await pool.connect();
-    const { rows, fields } = await connection.query('SELECT * FROM domeniistudiu WHERE id = $1', [id]);
+    const { rows, fields } = await pool.query('SELECT * FROM domeniistudiu WHERE id = $1', [id]);
     if (rows.length === 0) {
       res.status(404).json({ message: 'Domeniu not found' });
     } else {
@@ -20,8 +18,8 @@ export default async function handler(req, res) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
   } finally {
-    if (connection) {
-      connection.release();
+    if (pool) {
+      await pool.end();
     }
   }
 }
