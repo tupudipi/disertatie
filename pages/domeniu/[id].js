2e3673eebee6e500cf20479e662df142d68ba9d5
tupudipi <tupudipi@gmail.com>
2023-11-24 12:33:32 +0200

    "cosnt" typo

--- pages/domeniu/[id].js
+++ pages/domeniu/[id].js
@@ -10,7 +10,7 @@ function DomeniuPage() {
   const [domeniu, setDomeniu] = useState({});
   const router = useRouter();
   const { id } = router.query;
-  cosnt [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
   const pull_user = (user) => {
     setUser(user);
