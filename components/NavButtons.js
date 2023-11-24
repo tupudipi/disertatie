6170fc7353fa384350a0fc6b6184b1a778144325
tupudipi <tupudipi@gmail.com>
2023-11-24 10:50:21 +0200

    added useCallback to scope fetUser function

--- components/NavButtons.js
+++ components/NavButtons.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import RegistrationModal from './RegistratioModal';
 import LoginModal from './LoginModal';
 import { Button } from 'react-bootstrap';
@@ -7,25 +7,25 @@ import app from '../src/app/firebase';
 const NavButtons = () => {
     const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const response = await fetch('/api/currentUser');
-                if (response.ok) {
-                    const data = await response.json();
-                    setUser(data.user); // update state with user data
-                } else {
-                    // Handle error here
-                    setUser(null);
-                }
-            } catch (error) {
-                console.error('Error fetching current user:', error);
+    const fetchUser = useCallback(async () => {
+        try {
+            const response = await fetch('/api/currentUser');
+            if (response.ok) {
+                const data = await response.json();
+                setUser(data.user); // update state with user data
+            } else {
+                // Handle error here
                 setUser(null);
             }
-        };
+        } catch (error) {
+            console.error('Error fetching current user:', error);
+            setUser(null);
+        }
+    }, []);
 
+    useEffect(() => {
         fetchUser(); // fetch the current user when the component mounts
-    }, []); // empty dependency array means this effect runs once on mount and not on updates
+    }, [fetchUser]); // Include fetchUser in the dependency array
 
     const handleLogout = async () => {
         try {
@@ -33,7 +33,7 @@ const NavButtons = () => {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
             });
-        
+
             const data = await response.json();
             if (response.status === 200) {
                 console.log('User logged out...');
