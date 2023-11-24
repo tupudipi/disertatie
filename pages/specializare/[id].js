30738a01535ae974e4bfca22778e55eaceac8321
tupudipi <tupudipi@gmail.com>
2023-11-24 11:35:54 +0200

    imported useCallback

--- pages/specializare/[id].js
+++ pages/specializare/[id].js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useRouter } from 'next/router';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
