254bbf8e2623879d490c0a7ec8a3db818d297813
tupudipi <tupudipi@gmail.com>
2023-11-24 09:46:36 +0200

    refactor currentuser

--- pages/api/currentUser.js
+++ pages/api/currentUser.js
@@ -1,42 +1,38 @@
-// pages/api/currentuser.js
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import getPool from '../../lib/db';
 import app from '../../src/app/firebase';
 
 export default async function handler(req, res) {
-    if (req.method !== 'GET') {
-        return res.status(405).end();
-    }
+  if (req.method !== 'GET') {
+    return res.status(405).end();
+  }
 
-    const auth = getAuth(app);
-    onAuthStateChanged(auth, async (user) => {
+  const auth = getAuth(app);
+  const pool = getPool();
 
-        if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/auth.user
-            const uid = user.uid;
-            const email = user.email;
-            
-            // fetch user details from database
-            const pool = getPool();
-            try {
-                const {rows, fields} = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-                const userDetails = rows[0]; // Get the first result
+  onAuthStateChanged(auth, async (user) => {
+    try {
+      if (user) {
+        // User is signed in
+        const email = user.email;
 
-                if (!userDetails) {
-                    // No user found in database with the given email
-                    return res.status(404).json({ message: 'User not found' });
-                }
+        // fetch user details from database
+        const { rows, fields } = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        const userDetails = rows[0]; // Get the first result
 
-                return res.status(200).json({ user: { ...userDetails }});
-            } catch (error) {
-                console.error(error);
-                return res.status(500).json({ message: 'Internal server error' });
-            } 
-
-        } else {
-            // User is signed out
-            return res.status(401).json({ message: 'User is not signed in' });
+        if (!userDetails) {
+          // No user found in database with the given email
+          return res.status(404).json({ message: 'User not found' });
         }
-    });
+
+        return res.status(200).json({ user: { ...userDetails } });
+      } else {
+        // User is signed out
+        return res.status(401).json({ message: 'User is not signed in' });
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  });
 }
