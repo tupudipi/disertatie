d05e9595c727d488a3c74eb2f5b8af91834c66e8
tupudipi <tupudipi@gmail.com>
2023-11-25 16:28:18 +0200

    changed images on home page  to properly use next image component

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,28 @@
 // pages/HomePage.js
 'use client'
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from '../../components/Nav';
 import Footer from '../../components/Footer';
 import Image from 'next/image';
 import Link from 'next/link';
+import educationPic from '../../public/images/education.svg';
+import questionnairePic from '../../public/images/chestionar.svg';
+import chatbotPic from '../../public/images/chatbot.svg';
+import choosePic from '../../public/images/choose.svg';
+import webSearchPic from '../../public/images/web_search.svg';
+import publicDiscussionPic from '../../public/images/public_discussion.svg';
+import lightbulbMomentPic from '../../public/images/lightbulb_moment.svg';
 
 const HomePage = () => {
     const [user, setUser] = useState(null);
     const pull_user = (user) => {
-      setUser(user);
+        setUser(user);
     }
     return (
         <div>
-            <Nav pull_user={pull_user}/>
+            <Nav pull_user={pull_user} />
             <main style={{
                 paddingTop: '0px',
                 position: 'relative',
@@ -27,7 +34,7 @@ const HomePage = () => {
                 <Container fluid className="mb-5 shadow pt-5">
                     <Row>
                         <Col lg={6} className="text-center">
-                            <Image src="/images/education.svg" alt="" className="mw-100" />
+                            <Image src={educationPic} alt="" className="mw-100 mh-100 mx-0 my-0" style={{ width: 'clamp(388px, 100%, 744px)', height: 'auto',}} />
                         </Col>
                         <Col lg className="text-center my-auto">
                             <h3 className="p-3 display-6">Cauți un program de studii de licență potrivit exact preferințelor tale?</h3>
@@ -41,15 +48,13 @@ const HomePage = () => {
                         <Col lg={6} className="text-center my-auto">
                             <h3 className="p-3 display-6 font-weight-bold">Dacă nu ești sigur ce opțiuni ți se potrivesc cel mai bine, încearcă chestionarul nostru orientativ.</h3>
                             <Row className="mt-3 mb-4 justify-content-center">
-                            <Link href="/chestionar">
-                                  <a>
-                                        <Button variant="primary" size="lg" className="rounded-pill">Începe Chestionarul</Button>
-                                  </a>
-                            </Link>
+                                <Link href="/chestionar">
+                                    <Button variant="primary" size="lg" className="rounded-pill">Începe Chestionarul</Button>
+                                </Link>
                             </Row>
                         </Col>
                         <Col className="text-center">
-                            <Image src="/images/chestionar.svg" alt="Questionnaire Illustration" className="mw-100" />
+                            <Image src={questionnairePic} alt="" className="mw-100 mx-0 my-0" style={{ width: 'clamp(388px, 100%, 744px)', height: 'auto',}}/>
                         </Col>
                     </Row>
                 </Container>
@@ -57,16 +62,14 @@ const HomePage = () => {
                 <Container fluid className="mb-5 shadow">
                     <Row>
                         <Col lg={6} className="text-center">
-                            <Image src="/images/chatbot.svg" alt="AI Chat Illustration" className="mw-100" />
+                            <Image src={chatbotPic} alt="" className="mw-100 mx-0 my-0" style={{ width: 'clamp(388px, 100%, 744px)', height: 'auto',}}/>
                         </Col>
                         <Col lg className="text-center my-auto">
                             <h3 className="p-3 display-6 font-weight-bold">Apoi vorbește cu consilierul nostru educațional digital, care utilizează tehnologia AI pentru a oferi sfaturi personalizate:</h3>
                             <Link href="/chat">
-                              <a>
                                 <Button variant="outline-primary" size="lg" className="rounded-pill mb-3">
-                                  Chat cu Consilierul AI
+                                    Chat cu Consilierul AI
                                 </Button>
-                              </a>
                             </Link>
                         </Col>
                     </Row>
@@ -80,18 +83,18 @@ const HomePage = () => {
                             <Row className="mt-3 mb-4">
                                 <Col>
                                     <Link href="/domenii">
-                                        <a><Button variant="primary" size="lg" className="rounded-pill">Căutare după domeniu</Button></a>
+                                        <Button variant="primary" size="lg" className="rounded-pill">Căutare după domeniu</Button>
                                     </Link>
                                 </Col>
                                 <Col>
                                     <Link href="/regiuni">
-                                        <a><Button variant="primary" size="lg" className="rounded-pill">Căutare după locație</Button></a>
+                                        <Button variant="primary" size="lg" className="rounded-pill">Căutare după locație</Button>
                                     </Link>
                                 </Col>
                             </Row>
                         </Col>
                         <Col className="text-center">
-                            <Image src="/images/choose.svg" alt="" className="mw-100" />
+                            <Image src={choosePic} alt="" className="mw-100 mx-0 my-0" style={{ width: 'clamp(388px, 100%, 744px)', height: 'auto',}}/>
                         </Col>
                     </Row>
                 </Container>
@@ -99,12 +102,12 @@ const HomePage = () => {
                 <Container fluid className="mb-5 shadow">
                     <Row>
                         <Col lg={6} className="text-center">
-                            <Image src="/images/web_search.svg" alt="" className="mw-100" />
+                            <Image src={webSearchPic} alt="" className="mw-100 mx-0 my-0" style={{ width: 'clamp(388px, 100%, 744px)', height: 'auto',}}/>
                         </Col>
                         <Col lg className="text-center my-auto">
                             <h3 className="p-3 display-6 font-weight-bold">Pentru cei care vor să filtreze în detaliu opțiunile disponibile, există:</h3>
                             <Link href="/searchPage">
-                                <a><Button variant="outline-primary" size="lg" className="rounded-pill mb-3">Căutarea avansată!</Button></a>
+                                <Button variant="outline-primary" size="lg" className="rounded-pill mb-3">Căutarea avansată!</Button>
                             </Link>
                         </Col>
                     </Row>
@@ -117,7 +120,7 @@ const HomePage = () => {
                             <h3 className="p-3 display-6">În cazul în care nu găsești informația de care ai nevoie, <a href="#">înregistrează-te</a> și lasă o întrebare.</h3>
                         </Col>
                         <Col lg className="text-center">
-                            <Image src="/images/public_discussion.svg" alt="" className="mw-100" />
+                            <Image src={publicDiscussionPic} alt="" className="mw-100 mx-0 my-0" style={{ width: 'clamp(388px, 100%, 744px)', height: 'auto',}}/>
                         </Col>
                     </Row>
                 </Container>
@@ -125,7 +128,7 @@ const HomePage = () => {
                 <Container fluid className="shadow">
                     <Row>
                         <Col lg={6} className="text-center">
-                            <Image src="/images/lightbulb_moment.svg" alt="" className="mw-100" />
+                            <Image src={lightbulbMomentPic} alt="" className="mw-100 mx-0 my-0" style={{ width: 'clamp(388px, 100%, 744px)', height: 'auto',}}/>
                         </Col>
                         <Col lg className="text-center my-auto">
                             <h3 className="p-3 display-3 font-weight-bold">Începe căutarea și găsește-ți locul potrivit!</h3>
