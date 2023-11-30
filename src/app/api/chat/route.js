32890afe4ecb8506f0f4fb1ad27ec526d5e4232d
tupudipi <tupudipi@gmail.com>
2023-11-30 13:02:51 +0200

    Update route.js

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from 'openai';
 import { NextResponse } from 'next/server';
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
