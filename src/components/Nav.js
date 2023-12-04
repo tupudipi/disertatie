5051522cf5428e39bb3ec4f34a02af71cc457bca
tupudipi <tupudipi@gmail.com>
2023-12-04 14:36:09 +0200

    Add next/link to Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { Navbar, Nav, Button, Modal, Container } from 'react-bootstrap';
 import NavButtons from './NavButtons';
 import Image from 'next/image';
+import Link from 'next/link';
 import { AuthenticationProvider } from './context/AuthContext';
 
 const MyNav = () => {
@@ -17,22 +18,22 @@ const MyNav = () => {
         <Navbar.Collapse id="navbar-nav" >
           <Nav className="navbar-nav mx-auto border-bottom">
             <Nav.Item>
-              <Nav.Link href="/" className="text-center"><strong>Home</strong></Nav.Link>
+              <Nav.Link as={Link} href="/" className="text-center"><strong>Home</strong></Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="/chestionar" className="text-center"><strong>Chestionar</strong></Nav.Link>
+              <Nav.Link as={Link} href="/chestionar" className="text-center"><strong>Chestionar</strong></Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="/chat" className="text-center"><strong>Consilier Digital</strong></Nav.Link>
+              <Nav.Link as={Link} href="/chat" className="text-center"><strong>Consilier Digital</strong></Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="/domenii" className="text-center"><strong>Căutare după domeniu</strong></Nav.Link>
+              <Nav.Link as={Link} href="/domenii" className="text-center"><strong>Căutare după domeniu</strong></Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="/regiuni" className="text-center"><strong>Căutare după locație</strong></Nav.Link>
+              <Nav.Link as={Link} href="/regiuni" className="text-center"><strong>Căutare după locație</strong></Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="/search" className="text-center"><strong>Căutare avansată</strong></Nav.Link>
+              <Nav.Link as={Link} href="/search" className="text-center"><strong>Căutare avansată</strong></Nav.Link>
             </Nav.Item>
 
           </Nav>
